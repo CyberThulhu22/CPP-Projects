475c27a5d5ca7e7820921b60244b856fc745550e
CyberThulhu22 <37412977+CyberThulhu22@users.noreply.github.com>
2021-11-19 16:08:34 -0500

    Create _CPP_LEARNING__Hello_World.cpp

---
+++ 1_Practice_CPP/_CPP_LEARNING__Hello_World.cpp
@@ -0,0 +1,24 @@
+// Hello World Program
+// 
+// NAME:        helloworld.cpp
+// VERSION:     0.0.0
+// AUTHOR:      Jesse Leverett (CyberThulhu)
+// STATUS:      Work In Progress
+// DESCRIPTION: Basic Program That Prints Hello World
+// TO-DO:       Build Initial Code Framework
+// USAGE:       helloworld
+// COPYRIGHT © 2021 Jesse Leverett
+
+#include <iostream>
+
+using namespace std;
+
+// <summary>
+// Main Program that prints out 'Hello World'
+// and then prints 'This is awesome!'
+// </summary>
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "This " << "is " << "awesome!" << std::endl;
+    return 0;
+};
